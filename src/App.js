3b854bbf555a9e84627113231ced72739999e621
Ritik Gupta <ritikgupta1982@gmail.com>
2020-11-17 23:57:45 +0530

    updated app title, etc

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import SideBar from "./Components/SideBar";
 import Main from "./Components/Main";
@@ -7,24 +7,20 @@ import Navbar from "./Components/Navbar";
 const App = () => {
   return (
     <BrowserRouter>
-      <Fragment>
-        <div className='App'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-lg-3'>
-                <SideBar />
-              </div>
-              <div className='col-lg-9'>
-                <div className='main_content'>
-                  {/*Navbar starts Here*/}
-                  <Navbar />
-                  <Main />
-                </div>
-              </div>
+      <div className='App'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-lg-3'>
+              <SideBar />
+            </div>
+            <div className='col-lg-9 main_content'>
+              {/*Navbar starts Here*/}
+              <Navbar />
+              <Main />
             </div>
           </div>
         </div>
-      </Fragment>
+      </div>
     </BrowserRouter>
   );
 };
